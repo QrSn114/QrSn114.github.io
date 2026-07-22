2a04253d5e80eba12d94281b8f3bc33fbbe879dd
qrsn114 <2969158027@qq.com>
2026-07-22 21:28:54 +0800

    first commit

---
+++ valaxy.config.ts
@@ -0,0 +1,26 @@
+import type { UserThemeConfig } from 'valaxy-theme-yun'
+import { defineValaxyConfig } from 'valaxy'
+
+// add icons what you will need
+const safelist = [
+  'i-ri-home-line',
+]
+
+/**
+ * User Config
+ */
+export default defineValaxyConfig<UserThemeConfig>({
+  // site config see site.config.ts
+
+  theme: 'yun',
+
+  themeConfig: {
+    banner: {
+      enable: true,
+      title: '桂城中学民间新生指南',
+      cloud:{enable:true,},
+      duration:2000,
+    },
+  },
+  unocss: { safelist },
+})
